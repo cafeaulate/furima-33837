17c3b17a88b06ab6c663388d5267c340a752e4a9
cafeaulate <kira.conh@outlook.com>
2021-04-17 11:31:31 +0900

    devise導入、サインインページ編集

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :f_name, presence: true
+  validates :l_name, presence: true
+  validates :f_name_kana, presence: true
+  validates :l_name_kana, presence: true
+  validates :birthday, presence: true
+
+
+
+         
+end
