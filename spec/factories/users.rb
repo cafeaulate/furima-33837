0edfe7f4af0f06a4d9176e19629da9cfc3e0fe48
cafeaulate <kira.conh@outlook.com>
2021-04-24 19:45:18 +0900

    ユーザー管理機能修正２

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password }
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     f_name                { '太郎' }
     f_name_kana           { 'タロウ' }
