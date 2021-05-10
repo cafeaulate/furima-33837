aa84e1fd413254253ee1b3d50e04d613972ebcf2
cafeaulate <kira.conh@outlook.com>
2021-05-11 00:20:18 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: :edit
   def index
     @items = Item.all.order(created_at: :DESC)
   end
@@ -20,11 +21,29 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path, method: :get
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:product, :text, :price, :user_id, :category_id, :charge_id, :status_id, :prefecture_id,
                                  :span_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    redirect_to root_path if current_user.id != @item.user_id
   end
 end
