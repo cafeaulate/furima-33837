ddd55e5fe12da3b55bcbf6f645fef58c3706ed92
cafeaulate <kira.conh@outlook.com>
2021-05-29 15:39:21 +0900

    sold out表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :move_to_index_2 , only: :edit 
 
   def index
     @items = Item.all.order(created_at: :DESC)
@@ -53,5 +54,8 @@ class ItemsController < ApplicationController
   def move_to_index
     redirect_to root_path if current_user.id != @item.user_id
   end
-  
+
+  def move_to_index_2
+    redirect_to root_path if @item.order.present?
+  end
 end
